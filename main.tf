2f7529cee35dfc85737f200465c49546f8c59bb3
Gerald Kalafut <84546089+gerald-kalafut@users.noreply.github.com>
2024-06-04 14:32:24 -0700

    dups

--- main.tf
+++ main.tf
@@ -55,9 +55,6 @@ module "blog_alb" {
   subnets = module.blog_vpc.public_subnets
 
   load_balancer_type = "application"
-
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
   target_groups = [
