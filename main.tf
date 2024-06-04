11069efe61b25bb784c2cc24e35f31f10c3aa730
Gerald Kalafut <84546089+gerald-kalafut@users.noreply.github.com>
2024-06-04 15:03:52 -0700

    refactor using vars

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner] # Bitnami
 }
 
 data "aws_vpc" "default" {
@@ -21,26 +21,26 @@ data "aws_vpc" "default" {
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = "blog_vpc.${var.environment.name}"
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
-module "autoscaling" {
+module "blog_asg" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
-  name = "blog"
+  name = "blog-asg.${var.environment.name}"
 
-  min_size = 1
-  max_size = 2
+  min_size = var.asg_min_size
+  max_size = var.asg_max_size
 
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -53,7 +53,7 @@ module "autoscaling" {
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
-  name    = "blog-alb"
+  name    = "blog-alb.${var.environment.name}"
 
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
@@ -63,7 +63,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "blog-atg.${var.environment.name}"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -79,12 +79,12 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
 module "blog_sg" {
-  name    = "blog"
+  name    = "blog.${var.environment.name}"
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
 
@@ -95,4 +95,8 @@ module "blog_sg" {
 
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
+
+  tags = {
+    Environment = var.environment.name
+  }
 }
